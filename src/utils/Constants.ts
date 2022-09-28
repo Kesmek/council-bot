b29781e6cac510d80a05e2fa7ee1bc089261be9e
Justin <justin.scopelleti@gmail.com>
2022-09-28 02:59:10 -0400

    chore: Add Enum For Blacklist-able Reasons

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,3 +1,15 @@
 export const enum CommandActions {
-  Vouch= "Vouch",
+  Vouch = "Vouch",
+}
+
+export enum BlacklistReasons {
+  Pedophilia = "Pedophilia - Interacting with minors in a sexual fashion",
+  Negligence = "Negligence - Allowing minors access to adult content",
+  Blackmail = "Blackmail - Threatening to expose sensitive info in exchange" +
+    " for something",
+  Harassment = "Harassment/Crashing - Persistently bothering someone or" +
+    " crashing them",
+  Threats = "Threats - Serious threats of violence, doxxing or harrasment",
+  Nuking = "Nuking - Destroying established discord servers",
+  Underaged = "Underaged - Below 18 years old"
 }
