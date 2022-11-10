11338cd3928b78fcd1cd67ca35ba6f8de0b5c92d
Justin <justin.scopelleti@gmail.com>
2022-11-10 17:07:57 -0500

    feat: Initial Commit for Credits Command

---
+++ src/commands/Credits.ts
@@ -0,0 +1,18 @@
+import { Discord, Slash } from "discordx";
+import { CommandInteraction } from "discord.js";
+import { BotCreator } from "../utils/Constants.js";
+
+@Discord()
+class Credits {
+  @Slash({
+    name: "credits",
+    description: "Information about the creator of this bot",
+  })
+  async credits(interaction: CommandInteraction) {
+    await interaction.reply({
+      content: `This bot was created by ${BotCreator}. If you have any questions or suggestions,` +
+        ` please message them and voice your comments.`,
+      ephemeral: true,
+    });
+  }
+}
