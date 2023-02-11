df81077f1efe4bfafca1475e06ba2296d4e84b11
Justin <justin.scopelleti@gmail.com>
2023-02-11 15:47:03 -0500

    fix: Test Command Crash

--- src/commands/Test.ts
+++ src/commands/Test.ts
@@ -1,5 +1,5 @@
-import { Discord, Slash, SlashOption } from "discordx";
-import { ApplicationCommandOptionType, CommandInteraction, InviteGuild } from "discord.js";
+import { Discord, Slash } from "discordx";
+import { CommandInteraction } from "discord.js";
 import { injectable } from "tsyringe";
 import { InteractionUtils } from "../utils/Utils.js";
 
@@ -14,25 +14,15 @@ export class Test {
     description: "test stuff",
   })
   async credits(
-    @SlashOption({
-      name: "permanent-server-invite",
-      description: "A permanent invite to the server you own.",
-      type: ApplicationCommandOptionType.String,
-      required: true,
-    })
-    serverInvite: string,
     interaction: CommandInteraction
   ) {
     if (!interaction.inGuild()) {
       throw new Error("Command must be used within a guild!");
     }
 
-    const invite = (await interaction.client.fetchInvite(serverInvite));
-    const inviteGuild = invite.guild as InviteGuild;
-    console.log(invite);
-
     await InteractionUtils.replyOrFollowUp(interaction, {
-      content: `invite1: ${inviteGuild.name}, ${inviteGuild.nameAcronym}, ${inviteGuild.description}, ${inviteGuild.createdTimestamp}, ${inviteGuild.welcomeScreen}, ${invite.guild}`,
+      content: "This is a test",
+      ephemeral: true,
     });
   }
 }
